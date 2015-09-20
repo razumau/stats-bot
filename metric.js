265b32df0afae623d70e2d915b81b43ef07d77c9
Jury Razumau <jury.razumau@targetprocess.com>
2015-09-20 19:07:38 +0300

    parse response, format messages

--- metric.js
+++ metric.js
@@ -2,21 +2,22 @@
 // const events = require('eventemitter2');
 const rp = require('request-promise');
 const config = require('./config.js')
-const cron = require('cron');
+let cron = require('cron');
 
 class Metric {
     constructor(options, emitter) {
         this.name = options.name;
         this.emitter = emitter;
         this.requestOptions = {
-            url: config.body[0] + options.url + config.body[1],
-            method: "GET"
+        	url: "http://kibana.tpminsk.by/elasticsearch/_msearch",
+            body: config.body[0] + options.url + config.body[1],
+            method: "POST"
         };
         this.channel = options.channel;
         this.cronPattern = options.cronPattern;
         this.shouldRise = options.shouldRise;
 
-        let job = cron.CronJob(this.cronPattern, function() {console.log("asdf")}, null, true);
+        this.job = new cron.CronJob(this.cronPattern, this.getData.bind(this), null, true);
         //let job = cron.CronJob(this.cronPattern, this.getData, null, true);
 
             //setTimeout(this.getData.bind(this), this.timeout);
@@ -27,21 +28,35 @@ class Metric {
     }
 
     sendData(response) {
-        let data = {},
-            newValue = 5,
-            previousValue = 3;
+        let data = {};
+
+        let result = JSON.parse(response).responses[0].aggregations['1'];
+
+        let lastWeek = result.buckets[0]['2'].values['50.0'],
+        	thisWeek = result.buckets[1]['2'].values['50.0'],
+        	percentage = (thisWeek - lastWeek) / lastWeek * 100;
 
 
-        let result = JSON.parse(response);
+        data.text = `Last week: ${lastWeek.toFixed(3) * 1000} ms.
+This week: ${thisWeek.toFixed(3) * 1000} ms.
+*${this.percentageString(percentage)}*`;
 
-        let firstWeek = result.responses[0].aggregations['1'].buckets[0]['2'].values['50.0'];
 
-        data.text = `last week: ${firstWeek}, this week: ${newValue}.`;
-        data.mood = (newValue > previousValue) === this.shouldRise ? 'happy' : 'sad';
+        data.mood = Math.abs(percentage) < 5 
+        			? 'none'
+        			: (thisWeek > lastWeek) === this.shouldRise  
+        				? 'happy' 
+        				: 'sad';
         data.channel = this.channel;
         data.name = this.name;
 
         this.emitter.emit(`receive.${this.name}`, data);
+    }
+
+    percentageString(percentage) {
+    	return (percentage < 0)
+    			? `−${Math.abs(percentage.toFixed(1))}%`
+    			: `+${percentage.toFixed(1)}%`
     }
 }
 
