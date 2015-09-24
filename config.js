f71f1619b46f804dd9dc8807323fb155367cd8e0
Jury Razumau <jury.razumau@targetprocess.com>
2015-09-24 17:27:35 +0300

    post to performance channel

--- config.js
+++ config.js
@@ -1,56 +1,56 @@
 'use strict';
 const everyMonday = '0 11 * * 1';
-const test = '0 21 * * 1'
+const test = '45 12 * * 2'
 
 module.exports = {
 	metrics: [{
 		name: 'Cells median',
 		url: '/slice/v1/matrix/cells',
-		cronPattern: '0 2 * * *',
+		cronPattern: test,
 		shouldRise: false,
-		channel: "test-bot"
+		channel: "#performance"
 
 	}, {
 		name: 'CardsDetails median',
 		url: 'cardsDetails',
 		cronPattern: test,
 		shouldRise: false,
-		channel: "test-bot"
+		channel: "#performance"
 
 	}, {
 		name: 'GlobalDataTemplates median',
 		url: 'globalDataTemplates',
 		cronPattern: test,
 		shouldRise: false,
-		channel: "test-bot"
+		channel: "#performance"
 
 	}, {
 		name: 'AxesCounts median',
 		url: '/slice/v1/matrix/axesCounts',
 		cronPattern: test,
 		shouldRise: false,
-		channel: "test-bot"
+		channel: "#performance"
 
 	}, {
 		name: 'ViewDataTemplates median',
 		url: 'ViewDataTemplates',
 		cronPattern: test,
 		shouldRise: false,
-		channel: "test-bot"
+		channel: "#performance"
 
 	}, {
 		name: 'UserStories median',
 		url: '/api/v1/UserStories',
 		cronPattern: test,
 		shouldRise: false,
-		channel: "test-bot"
+		channel: "#performance"
 
 	}, {
 		name: 'Context median',
 		url: '/api/v2/context',
 		cronPattern: test,
 		shouldRise: false,
-		channel: "test-bot"
+		channel: "#performance"
 
 	}/*, {
 		name: 'View list median',
