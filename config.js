a3fcb18af6c0314fda0a53f43cf73c5e6b425c48
Jury Razumau <jurazumau@gmail.com>
2015-10-12 23:28:46 +0300

    add more metrics

--- config.js
+++ config.js
@@ -1,55 +1,131 @@
 'use strict';
 const everyMonday = '0 12 * * 1';
-const test = '25 12 * * 1'
+const test = '0 51 11 * * *'
+const test2 = '30 51 11 * * *'
+const test3 = '0 52 11 * * *'
+const test4 = '30 52 11 * * *'
 
 module.exports = {
 	metrics: [{
 		name: 'Cells',
-		url: '/slice/v1/matrix/cells',
+		searchString: 'uri:\\"/slice/v1/matrix/cells\\"',
 		cronPattern: test,
 		shouldRise: false,
-		channel: "#performance"
-	}, {
+		kibana: 'logstash',
+		performance: true
+	},
+	 {
 		name: 'CardsDetails',
-		url: 'cardsDetails',
+		searchString: 'uri:\\"cardsDetails\\"',
 		cronPattern: test,
 		shouldRise: false,
-		channel: "#performance"
+		kibana: 'logstash',
+		performance: true
 
 	}, {
 		name: 'GlobalDataTemplates',
-		url: 'globalDataTemplates',
+		searchString: 'uri:\\"globalDataTemplates\\"',
 		cronPattern: test,
 		shouldRise: false,
-		channel: "#performance"
+		kibana: 'logstash',
+		performance: true
 
 	}, {
 		name: 'AxesCounts',
-		url: '/slice/v1/matrix/axesCounts',
+		searchString: 'uri:\\"/slice/v1/matrix/axesCounts\\"',
 		cronPattern: test,
 		shouldRise: false,
-		channel: "#performance"
+		kibana: 'logstash',
+		performance: true
 
 	}, {
 		name: 'ViewDataTemplates',
-		url: 'ViewDataTemplates',
-		cronPattern: test,
+		searchString: 'uri:\\"ViewDataTemplates\\"',
+		cronPattern: test2,
 		shouldRise: false,
-		channel: "#performance"
+		kibana: 'logstash',
+		performance: true
 
 	}, {
 		name: 'UserStories',
-		url: '/api/v1/UserStories',
-		cronPattern: test,
+		searchString: 'uri:\\"/api/v1/UserStories\\"',
+		cronPattern: test2,
 		shouldRise: false,
-		channel: "#performance"
+		kibana: 'logstash',
+		performance: true
 
 	}, {
 		name: 'Context',
-		url: '/api/v2/context',
-		cronPattern: test,
+		searchString: 'uri:\\"/api/v2/context\\"',
+		cronPattern: test2,
 		shouldRise: false,
-		channel: "#performance"
-
-	}]
+		kibana: 'logstash',
+		performance: true
+	}, {
+		name: 'Xaxis',
+		searchString: 'uri:\\"/slice/v1/matrix/xaxis\\"',
+		cronPattern: test2,
+		shouldRise: false,
+		kibana: 'logstash',
+		performance: true
+	}, {
+		name: 'Yaxis',
+		searchString: 'uri:\\"/slice/v1/matrix/yaxis\\"',
+		cronPattern: test3,
+		shouldRise: false,
+		kibana: 'logstash',
+		performance: true
+	}, {
+		name: 'GeneralFollowers',
+		searchString: 'uri:\\"/api/v1/generalFollowers\\"',
+		cronPattern: test3,
+		shouldRise: false,
+		kibana: 'logstash',
+		performance: true
+	}, {
+		name: 'TreeView',
+		searchString: 'uri:\\"/slice/v1/TreeView/treeView\\"',
+		cronPattern: test3,
+		shouldRise: false,
+		kibana: 'logstash',
+		performance: true
+	}, {
+		name: 'TreePossibleActions',
+		searchString: 'uri:\\"/slice/v1/TreeView/treePossibleActions\\"',
+		cronPattern: test3,
+		shouldRise: false,
+		kibana: 'logstash',
+		performance: true
+	}, {
+		name: 'Report',
+		searchString: 'uri:\\"/slice/v1/report\\"',
+		cronPattern: test4,
+		shouldRise: false,
+		kibana: 'logstash',
+		performance: true
+	}, {
+		name: 'Timeline density',
+		searchString: 'uri:\\"/slice/v1/timeline/density\\"',
+		cronPattern: test4,
+		shouldRise: false,
+		kibana: 'logstash',
+		performance: true
+	}, {
+		name: 'Milestones',
+		searchString: 'uri:\\"/slice/v1/timeline/milestones\\"',
+		cronPattern: test4,
+		shouldRise: false,
+		kibana: 'logstash',
+		performance: true
+	}
+	// }, {
+	// 	name: 'Initial load time',
+	// 	searchString: '*',
+	// 	cronPattern: test2,
+	// 	shouldRise: false,
+	// 	kibana: 'taus',
+	// 	index: 'tauspy-initial.load.times',
+	// 	performance: true
+	// }
+	]
 };
